d54f869e90d7e60a175d529f6addcbb7e7485f62
Saleh AL-Hourani <37269217+Saleh-alhourani@users.noreply.github.com>
2022-11-24 15:49:09 -0800

    Update configVPS.sh

--- configVPS.sh
+++ configVPS.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-## bash script for configering a Testing VPS
+##  Written by Saleh Al-Hourni (https://www.linkedin.com/in/saleh-alhourani) 
  
+## bash script for configering a ubuntu 22.04 VPS 
 
 # updating  linux
 apt update -y
@@ -31,10 +32,11 @@ export PATH=$PATH:/usr/local/go/bin
 
 
 # configuring security tools
-# install  nmap , eyewitness , arjun
+# install  nmap , eyewitness , arjun , amass 
 apt install  nmap   eyewitness   -y
 pip3 install arjun
 
+snap install amass 
 ## installing Metasploit
 curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall
 chmod 755 msfinstall
@@ -61,3 +63,7 @@ cd /opt/wordlist
 wget -c https://github.com/hAPI-hacker/Hacking-APIs/archive/refs/heads/main.zip -O HackingAPIs.zip \
 &&  unzip HackingAPIs.zip \
 &&  rm -f HackingAPIs.zip
+
+cd /opt/wordlist 
+wget https://gist.github.com/jhaddix/f64c97d0863a78454e44c2f7119c2a6a/raw/96f4e51d96b2203f19f6381c8c545b278eaa0837/all.txt 
+
