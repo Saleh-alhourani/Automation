8e9783f8481ac045d6ab3799146549514d076662
Saleh AL-Hourani <37269217+Saleh-alhourani@users.noreply.github.com>
2022-11-24 16:04:24 -0800

    Update configVPS.sh

--- configVPS.sh
+++ configVPS.sh
@@ -65,5 +65,4 @@ wget -c https://github.com/hAPI-hacker/Hacking-APIs/archive/refs/heads/main.zip 
 &&  rm -f HackingAPIs.zip
 
 cd /opt/wordlist 
-wget https://gist.github.com/jhaddix/f64c97d0863a78454e44c2f7119c2a6a/raw/96f4e51d96b2203f19f6381c8c545b278eaa0837/all.txt 
-
+wget https://gist.githubusercontent.com/jhaddix/f64c97d0863a78454e44c2f7119c2a6a/raw/96f4e51d96b2203f19f6381c8c545b278eaa0837/all.txt
